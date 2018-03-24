1d8057a8dd25b9065a3ab1711024037ae1510d46
Deniss Celuiko <deniss.celuiko@gmail.com>
2018-03-24 02:34:23 +0200

    Made MattermostController handle the initial request better and split arguments passed in 'text'. Removed argument when calling optionHelp.

--- app/Http/Controllers/Mattermost/MattermostController.php
+++ app/Http/Controllers/Mattermost/MattermostController.php
@@ -8,32 +8,55 @@ use Illuminate\Http\Request;
 class MattermostController extends Controller
 {
     protected $request;
+    protected $args = null;
+    protected $option = null;
     protected $helpMessage = 'Placeholder help message';
 
 
     public function __construct(Request $request)
     {
         $this->request = $request;
+        $this->splitText();
     }
 
     /**
      * Route a user's request.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function router()
     {
         if ($this->request->has('text'))
         {
-            $options = explode(' ', $this->request->text);
-            $method = 'option'.$options[0];
+            $method = 'option'.$this->option;
 
             if (method_exists($this, $method))
             {
                 return $this->$method($this->request);
             }
         }
-        return $this->optionHelp($this->request);
+        return $this->optionHelp();
+    }
+
+    /**
+     * Splits 'text' argument from Mattermost.
+     */
+    protected function splitText() {
+        if ($this->request->has('text') && mb_strlen($this->request->text) > 0)
+        {
+            preg_match_all("/(\w+)|\"([^\"]+)\"/u", $this->request->text, $matches);
+            $matches = $matches[0]; // We only need full pattern matches.
+
+            // Cleanup
+            foreach ($matches as &$match)
+            {
+                $match = trim($match, ' "');
+            }
+
+            $this->option = $matches[0] ?? null;
+            $this->args = array_slice($matches, 1);
+        }
+
     }
 
     /**
