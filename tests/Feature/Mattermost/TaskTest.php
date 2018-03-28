089860ad4d1aaeb9183f3952dc159784191ebb6e
Deniss Celuiko <deniss.celuiko@gmail.com>
2018-03-28 18:58:48 +0300

    Added factory for Task Added TaskTest

--- tests/Feature/Mattermost/TaskTest.php
+++ tests/Feature/Mattermost/TaskTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature\Mattermost;
 
+use App\Project;
+use App\Task;
+use App\Team;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\MattermostRequest;
 use Tests\TestCase;
@@ -13,11 +16,22 @@ class TaskTest extends TestCase
     use DatabaseTransactions;
     use MattermostRequest;
 
+    /**
+     * @var Team
+     */
+    protected $team;
+
+    /**
+     * @var Project
+     */
+    protected $project;
+
     public function setUp()
     {
         parent::setUp();
 
-
+        $this->team = factory(Team::class)->create();
+        $this->project = $this->team->projects()->save(factory(Project::class)->make());
     }
 
     /**
@@ -25,6 +39,49 @@ class TaskTest extends TestCase
      */
     public function can_create_task()
     {
-//        $this->send('/t');
+        $task = factory(Task::class)->make();
+        $response = $this->text("new {$this->project->code} $task->name")->team($this->team)->send('/t');
+
+        $response->assertSee("$task->name created");
     }
+
+
+    /**
+     * @test
+     */
+    public function increments_next_task_number()
+    {
+        $task = factory(Task::class)->make();
+        $this->project = $this->project->fresh();
+        $this->text("new {$this->project->code} $task->name")->team($this->team)->send('/t');
+
+        $this->assertDatabaseHas('projects',[
+            'next_task_number' => $this->project->next_task_number+1,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function must_have_arguments()
+    {
+        $response = $this->text("new")->team($this->team)->send('/t');
+
+        $response->assertSee("Usage");
+    }
+
+    /**
+     * @test
+     */
+    public function rejects_not_existing_projects()
+    {
+        $task = factory(Task::class)->make();
+        $code = substr(md5($this->project->code), 0, 4);
+
+        $response = $this->text("new $code $task->name")->team($this->team)->send('/t');
+
+        $response->assertJsonFragment(['text' => "Project with a code `$code` does not exist."]);
+    }
+
+
 }
