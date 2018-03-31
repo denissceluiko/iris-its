7c9b30d9cf930393f0a41793ac4985de91a37b0b
Deniss Celuiko <deniss.celuiko@gmail.com>
2018-03-31 05:06:57 +0300

    Capitalization

--- resources/views/mattermost/task/list.blade.php
+++ resources/views/mattermost/task/list.blade.php
@@ -5,6 +5,6 @@ Tasks in {{ $project->name }} of {{ $team->mm_domain }}
 | Code    | Name              | Status | Assigned to | Created By | Deadline |
 | :------ | :---------------- | ------ | ----------- | ---------- | -------: |
 @foreach($tasks as $task)
-| {{ $task->code }} | {{ $task->name }} | {{ $task->status }} | {{ ($task->assignee ? $task->assignee->name : '-') }} | {{ $task->creator->name }} |  {{ ( $task->deadline_at ? $task->deadline_at->format('d.m.Y.') : '-' ) }} |
+| {{ $task->code }} | {{ $task->name }} | {{ ucfirst($task->status) }} | {{ ($task->assignee ? $task->assignee->name : '-') }} | {{ $task->creator->name }} |  {{ ( $task->deadline_at ? $task->deadline_at->format('d.m.Y.') : '-' ) }} |
 @endforeach
 @endsection
